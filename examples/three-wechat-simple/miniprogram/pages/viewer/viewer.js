a2adf38c2d4301bcf117f0edbc22a2223ee17d23
wj.wei <>
2023-01-12 12:18:36 +0800

    update

--- examples/three-wechat-simple/miniprogram/pages/viewer/viewer.js
+++ examples/three-wechat-simple/miniprogram/pages/viewer/viewer.js
@@ -6790,15 +6790,15 @@ class Scene2 extends Viewer {
         const hdrPath = publishPath + scene.skyboxHDR;
         const assetDatas = {
             "kirinModel": {
-                url: "https://emw-pub.uality.cn/qohsaw5u_p32/4/qohsaw5u_p32_sceneViewer.glb",
+                url: "https://emw-pub.uality.cn/qohsaw5u_p32/6/qohsaw5u_p32_sceneViewer.glb",
                 type: "gltf"
             },
             "jingyuModel": {
-                url: "https://emw-pub.uality.cn/qcnolauw_qg5/4/qcnolauw_qg5_sceneViewer.glb",
+                url: "https://emw-pub.uality.cn/qcnolauw_qg5/5/qcnolauw_qg5_sceneViewer.glb",
                 type: "gltf"
             },
             "fangziModel": {
-                url: "https://emw-pub.uality.cn/5cvlv4xo_kzb/7/5cvlv4xo_kzb_sceneViewer.glb",
+                url: "https://emw-pub.uality.cn/5cvlv4xo_kzb/9/5cvlv4xo_kzb_sceneViewer.glb",
                 type: "gltf"
             },
             "hdr": {
